1c59dfa7bb1eb7b021f950873112d9005c19fb19
Omar Allawala <allawala@Omars-MacBook-Pro.local>
2018-08-19 13:31:56 +1000

    Minimal configuration

---
+++ src/main/resources/logback.sandbox.groovy
@@ -0,0 +1,16 @@
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder
+
+// See http://www.solutionsiq.com/implementing-structured-logging-in-groovy/
+
+def appenders = ["CONSOLE"]
+
+
+appender("CONSOLE", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%d{HH:mm:ss.SSS} [%X{X-CORRELATION-ID}] [%thread] %-5level %logger{36} - %msg%n"
+    }
+}
+
+logger("org.apache.shiro", INFO)
+
+root(DEBUG, appenders)
